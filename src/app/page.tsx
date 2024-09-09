e7725725f156aa2823283a04c865dbc8670c35c2
Bahram <Bahram_bb@hotmail.com>
2024-09-09 14:33:06 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,12 +150,12 @@ const data: Transaction[] = [
 export default function Home() {
   const { setTheme } = useTheme()
   return (
-    <div className=""> <header className="flex h-14 bg-black justify-end items-center bg-background px-4 lg:px-6"><DropdownMenu>
+    <div className=""> <header className="flex h-14 justify-end items-center bg-background px-4 lg:px-6"><DropdownMenu>
     <DropdownMenuTrigger asChild>
       <Button variant="outline" size="icon">
         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        <span className="sr-only">Toggle theme</span>
+        <span className="sr-only">Toggle theme BBBBB</span>
       </Button>
     </DropdownMenuTrigger>
     <DropdownMenuContent align="end">
